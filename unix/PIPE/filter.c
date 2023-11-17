47257459844b66d7a207980f34a111373e3978a8
luca <lucabk04@gmail.com>
2023-11-17 20:16:41 +0100

    second commit from vscode

--- unix/PIPE/filter.c
+++ unix/PIPE/filter.c
@@ -1,3 +1,19 @@
 //
 // Created by Luca Betti on 17/11/23.
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main() {
+    int c;
+    while ((c = getchar()) != EOF) {
+        if(c == EOF){
+            printf("Output erro");
+            exit(EXIT_FAILURE);
+        }
+        putchar(toupper(c));
+    }
+    return 0;
+}
+
--- unix/PIPE/main_pipe.c
+++ unix/PIPE/main_pipe.c
@@ -1,3 +1,39 @@
 //
 // Created by Luca Betti on 17/11/23.
 //
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    int pipefd[2];
+    pid_t pid;
+    char buf;
+
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        return 1;
+    }
+
+    pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        return 1;
+    }
+
+    if (pid == 0) {
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        execlp("./filter", "./filter", NULL);
+        perror("execlp");
+        return 1;
+    } else {
+        close(pipefd[1]);
+        while (read(pipefd[0], &buf, 1) > 0) {
+            putchar(buf);
+        }
+        wait(NULL);
+    }
+
+    return 0;
+}
