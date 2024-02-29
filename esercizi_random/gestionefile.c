3067ab66191424ba3e952a155c072e1651fd9760
Luca <53485931+KeryWT@users.noreply.github.com>
2024-02-29 17:03:22 +0100

    Create gestionefile.c

---
+++ esercizi_random/gestionefile.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+#include "unistd.h"
+#include <stdlib.h>
+int main() {
+  // apri un file di testo in scrittura
+  FILE *f = fopen("test.txt", "w");
+
+  pid_t pid = fork();
+  if (pid == -1) {
+    printf("Errore nella creazione del processo figlio\n");
+    exit(-1);
+  } else if (pid == 0) {
+    // siamo nel figlio
+    // scrivi nel file
+    fprintf(f, "Testo a caso scritto dal figlio.\n");
+    fclose(f); // chiudi il file
+    exit(0);
+  } else {
+    // siamo nel padre
+    wait(NULL);
+    // aspetta l'esecuzione del figlio, poi apri il file e stampane il contenuto
+    f = fopen("test.txt", "r");
+    // stampa il contenuto del file
+    char c;
+    while ((c = fgetc(f)) != EOF) {
+      printf("%c", c);
+    }
+    fclose(f); // chiudi il file
+  }
+}
