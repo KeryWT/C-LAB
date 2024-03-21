7f3af3be4ff178dbbf4f95f4a7add1dec67e1f5a
Luca <lucabk04@gmail.com>
2024-03-21 09:47:10 +0100

    Updated and corrected sum-rand-kids

--- eserciziT2/sum-rand-kids.c
+++ eserciziT2/sum-rand-kids.c
@@ -2,27 +2,49 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
 
 #define NUM_KIDS 20
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+  int status = 0;
   int sum = 0;
-  for (int i = 0; i < NUM_KIDS; i++) {
-    int pid = fork();
-    if (pid == 0) {
-        srand(getpid());
-      int n = rand() % (6 + 1 - 1) + 1;
+  for (int i = 0; i < NUM_KIDS; i++)
+  {
+    switch (fork())
+    {
+    case -1:
+      /* Handle error */
+      fprintf(stderr, "%s, %d: Errore (%d) nella fork\n",
+              __FILE__, __LINE__, errno);
+      exit(EXIT_FAILURE);
+      break;
+
+    case 0:
+      // CHILD CODE
+      srand(getpid());
+      int n = arc4random() % (6 + 1 - 1) + 1;
       printf(
           "I am the child #%d with pid %d and I generated this random number: %d\n",
           i, getpid(), n);
       exit(n);
-    } else if (pid != 0) {
-      int status;
-      wait(&status);
-      sum += WEXITSTATUS(status);
-    } else {
-      printf("Error creating child %d\n", i);
+    default:
+      // PARENT CODE: nothing here
+      break;
     }
   }
-  printf("I am the Father and the sum of the random numbers is: %d\n", sum);
+
+  while (wait(&status) != -1)
+  {
+    /*
+     * Estrai l'exit status dei figli e somma in sum
+     */
+    sum += WEXITSTATUS(status);
+  }
+
+  printf("Number of childs: %d\n", NUM_KIDS);
+  printf("Sum of the random numbers: %d\n", sum);
+  exit(0);
 }
