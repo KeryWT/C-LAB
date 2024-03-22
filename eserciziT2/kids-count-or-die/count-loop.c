834b9656e45a13af7362bd5e48395c8a6c871619
Luca Betti <lucabk04@gmail.com>
2024-03-23 00:04:07 +0100

    Fix kids-count-or-die exercise

--- eserciziT2/kids-count-or-die/count-loop.c
+++ eserciziT2/kids-count-or-die/count-loop.c
@@ -2,17 +2,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <errno.h>
 
-int main(int argc, char *argv[]){ // dobbiamo ricevere due argomenti. Primo:una durata TIMEOUT, Secondo:numero n_iter  di iterazioni
-    if(argc != 3){
+long strtol_err(char *string)
+{
+    long ret_var = strtol(string, NULL, 10);
+    if (ret_var == 0 && errno != 0)
+    {
+        fprintf(stderr, "Error in converting %s into a number", string);
+        exit(-1);
+    }
+    return ret_var;
+}
+
+int main(int argc, char **argv)
+{ // dobbiamo ricevere due argomenti. Primo:una durata TIMEOUT, Secondo:numero n_iter  di iterazioni
+    if (argc != 3)
+    {
         printf("Usage: %s <timeout> <number of Iterations>\n", argv[0]);
         exit(1);
     }
-    int timeout = atoi(argv[1]);
-    int n_iter = atoi(argv[2]);
+    long timeout = strtol_err(argv[1]);
+    long n_iter = strtol_err(argv[2]);
     alarm(timeout);
 
-    for(int i=0; i<n_iter; i++);
-    printf("Child with PID: %d has finished executing the count-loop file\n", getpid());
+    for (long i = 0; i < n_iter; i++)
+        ;
     return 0;
 }
--- eserciziT2/kids-count-or-die/kids-count-or-die.c
+++ eserciziT2/kids-count-or-die/kids-count-or-die.c
@@ -3,44 +3,123 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <errno.h>
 
-int main(int argc, char *argv[])
+#define BUFFER 50
+
+struct child
 {
-  if (argc != 4)
-  {
-    printf("Usage: %s <timeout> <number of Iterations> <number of kids>\n",
-           argv[0]);
-    exit(1);
-  }
-  char *arguments[] = {"./count-loop.out", argv[1], argv[2], NULL};
-  int n_kids = atoi(argv[3]);
+    pid_t pid;
+    long iter;
+    int rerun;
+};
 
-  for (int i = 0; i < n_kids; i++)
-  {
-    pid_t pid = fork();
-    if (pid == 0)
+long strtol_err(char *string);
+void run_child(long timeout, long n_iter);
+int lookup_arr(struct child *, int size, pid_t pid);
+
+int main(int argc, char **argv)
+{
+    if (argc != 4)
     {
-      if (execve("./count-loop.out", arguments, NULL) == -1)
-      {
-        printf("Error in execve\n");
+        printf("Usage: %s <timeout> <number of Iterations> <number of kids>\n",
+               argv[0]);
         exit(1);
-      }
     }
-    else if (pid != -1)
+
+    long timeout, n_iter, n_kids;
+    int i, status;
+    struct child *children;
+    pid_t pid;
+
+    timeout = strtol_err(argv[1]);
+    n_iter = strtol_err(argv[2]);
+    n_kids = strtol_err(argv[3]);
+
+    children = malloc(sizeof(*children) * n_kids);
+
+    for (i = 0; i < n_kids; i++)
     {
-      int status;
-      wait(&status);
-      if (WIFSIGNALED(status))
-      {
-        long n_iter = atoi(argv[2]) / 2;
-        char *arguments[] = {"./count-loop.out", argv[1], (char *)n_iter, NULL};
-        printf("A child count-loop has been interrupted due to the alarm.\n");
-      }
+        children[i].iter = n_iter;
+        children[i].pid = fork();
+        children[i].rerun = 0;
+
+        if (children[i].pid == -1) // error handling
+        {
+            printf("Error in fork\n");
+            exit(1);
+        }
+        else if (children[i].pid == 0) // CHILD CODE
+        {
+            run_child(timeout, n_iter);
+        }
     }
-    else
+
+    while ((pid = wait(&status)) != -1)
     {
-      printf("Error in fork\n");
-      exit(1);
+        if (WIFSIGNALED(status))
+        {
+            printf("Child %d was killed by signal, ", pid);
+            int i = lookup_arr(children, n_kids, pid);
+
+            children[i].iter >>= 1;
+            children[i].pid = fork();
+            children[i].rerun++;
+
+            if (children[i].pid == 0)
+            {
+                run_child(timeout, children[i].iter);
+            }
+            printf("now it's %d and will run for %ld iterations\n", children[i].pid, children[i].iter);
+        }
     }
-  }
+
+    for (int i = 0; i < n_kids; i++)
+    {
+        printf("Child %d, iterations: %ld iterations, rerun %d times\n", children[i].pid, children[i].iter, children[i].rerun);
+    }
+
+    free(children);
+}
+
+void run_child(long timeout, long n_iter)
+{
+    char timeout_str[BUFFER];
+    char n_iter_str[BUFFER];
+    char *args[4];
+    args[0] = "";
+    args[1] = timeout_str;
+    args[2] = n_iter_str;
+    args[3] = NULL;
+
+    snprintf(timeout_str, BUFFER - 1, "%ld", timeout);
+    snprintf(n_iter_str, BUFFER - 1, "%ld", n_iter);
+
+    execve("count-loop", args, NULL);
+
+    perror("");
+
+    exit(-2);
+}
+
+int lookup_arr(struct child *children, int size, pid_t pid)
+{
+    int i = 0;
+    for (i = 0; i < size; i++)
+    {
+        if (children[i].pid == pid)
+            return i;
+    }
+    return -1;
+}
+
+long strtol_err(char *string)
+{
+    long ret_var = strtol(string, NULL, 10);
+    if (ret_var == 0 && errno != 0)
+    {
+        fprintf(stderr, "Error in converting %s into a number", string);
+        exit(-1);
+    }
+    return ret_var;
 }
